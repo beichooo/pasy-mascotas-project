5b2f4af69cd3a9050b7789d52cfa1ff68eb7f370
beicho <be.imagine.redo@gmail.com>
2023-08-24 17:16:25 -0400

    fix the debug mode for bucket

--- pasy_beta/settings.py
+++ pasy_beta/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv("DEBUG", "False") == "True"
-DEBUG = "True"
+DEBUG = os.getenv("DEBUG", "False") == "True"
+# DEBUG = "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
